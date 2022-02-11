0f071c2f0db4af414e592282d51642353f704123
bbubam <benietc1106@gmail.com>
2022-02-11 21:02:32 +0700

    v1.1.0

---
+++ src/main/java/event_auto.java
@@ -0,0 +1,33 @@
+import org.sikuli.script.Screen;
+
+public class event_auto {
+    public void run(Screen screen, String strImagePath, int intTotalLoop, int intWaitTime)throws Exception{
+         for (int i = 0; i < intTotalLoop; i++) {
+            umamusume.clickImage(screen, strImagePath , "imgUmamusume" + ".png");
+            umamusume.clickImage(screen,strImagePath , "btnSelectSummon" + ".png");
+            umamusume.clickImage(screen,strImagePath , "btnOK" + ".png");
+            Thread.sleep(intWaitTime);
+
+            if(umamusume.isExistScreen(screen,strImagePath , "imgElixir" + ".png")){
+                umamusume.waitUntilImage(screen,strImagePath , "btnUse" + ".png");
+                umamusume.useElixir(screen,strImagePath , "btnUse" + ".png");
+                umamusume.clickImage(screen,strImagePath , "btnOK" + ".png");
+            }
+
+            if(umamusume.isRelicBuster){
+                if(umamusume.isExistScreen(screen,strImagePath , "btnOugiOff" + ".png")){
+                    umamusume.clickImage(screen,strImagePath , "btnOugiOff" + ".png");
+                }
+                umamusume.clickImage(screen,strImagePath , "imgDjeetaRelicBuster" + ".png");
+                umamusume.clickImage(screen,strImagePath , "btnEngage" + ".png");
+                umamusume.clickImage(screen,strImagePath , "btnLimitBurst" + ".png");
+            }
+            umamusume.clickImage(screen,strImagePath , "btnAtk" + ".png");
+            umamusume.clickImage(screen,strImagePath , "btnSemiAuto" + ".png");
+            umamusume.waitUntilImage(screen,strImagePath , "btnOK" + ".png");
+            umamusume.clickImage(screen,strImagePath , "btnOK" + ".png");
+        }
+    }
+
+}
+
--- src/main/java/umamusume.java
+++ src/main/java/umamusume.java
@@ -1,41 +1,67 @@
+import org.python.google.common.base.Stopwatch;
 import org.sikuli.script.*;
+import org.springframework.util.StopWatch;
 
-import java.util.Iterator;
+import java.io.FileInputStream;
+import java.time.Duration;
 import java.util.List;
+import java.util.Properties;
+import java.util.Timer;
 
 public class umamusume {
+
+    public static boolean isRelicBuster;
+    public static int intTotalLoop;
+    public static int intWaitTime;
+
     public static void main(String[] args)throws Exception {
         String[] strPath = umamusume.class.getProtectionDomain().getCodeSource().getLocation().getPath().split("/");
         String strImagePath = getPathExisting(strPath) + "image\\";
+        Properties prop = new Properties();
+        String strConfigName = "config.cfg";
+        loadConfig(prop,strConfigName);
         Screen screen = new Screen();
-
-        for (int i = 0; i < 100; i++) {
-            clickImage(screen, strImagePath , "imgUmamusume" + ".png");
-            clickImage(screen,strImagePath , "btnSelectSummon" + ".png");
-            Thread.sleep(1000);
-            clickImage(screen,strImagePath , "btnOK" + ".png");
-            Thread.sleep(1000);
-            if(isExistScreen(screen,strImagePath , "imgElixir" + ".png")){
-                clickImage(screen,strImagePath , "btnUse" + ".png");
-                clickImage(screen,strImagePath , "btnOK" + ".png");
-            }
-            clickImage(screen,strImagePath , "btnAtk" + ".png");
-            clickImage(screen,strImagePath , "btnSemiAuto" + ".png");
-            clickImage(screen,strImagePath , "btnOK" + ".png");
+        String strAutomationType = prop.get("automation_type").toString();
+        intTotalLoop = Integer.parseInt(prop.get("total_loop").toString());
+        intWaitTime = Integer.parseInt(prop.get("wait_time").toString());
+        isRelicBuster = Boolean.parseBoolean(prop.get("Relic_Buster").toString());
+        if(strAutomationType.equals("event_auto")){
+            event_auto objEventAuto = new event_auto();
+            objEventAuto.run(screen, strImagePath, intTotalLoop, intWaitTime);
+        }else if(strAutomationType.equals("raid_auto")){
+            raid_auto objRaidAuto = new raid_auto();
+            objRaidAuto.run(screen, strImagePath, intTotalLoop, intWaitTime);
         }
     }
 
-    private static void clickImage(Screen screen, String imgPath, String imgName)throws Exception {
+    public static void useElixir(Screen screen, String imgPath, String imgName){
         String img = imgPath+imgName;
         boolean isClicked = false;
         while (!isClicked) {
             try {
+                List<Match> arrayMatch = screen.findAllByRow(new Pattern(img));
+                Match matchImage = arrayMatch.get(1);
+                matchImage.mouseMove();
+                matchImage.click();
+                isClicked = true;
+            } catch (Exception e) {
+                System.out.println("Retrying to click the image..." + imgName);
+            }
+        }
+    }
+
+    public static void clickImage(Screen screen, String imgPath, String imgName)throws Exception {
+        String img = imgPath+imgName;
+        StopWatch stopWatch = new StopWatch();
+        boolean isClicked = false;
+        stopWatch.start();
+        while (!isClicked && stopWatch.getTotalTimeSeconds() <= 10.00) {
+            try {
                 if(img.contains("btnUse")){
-                   Iterator<Match> iterMatch = screen.findAll(new Pattern(img));
-                   while(iterMatch.hasNext()){
-                     iterMatch.next().mouseMove();
-                     iterMatch.next().click();
-                    }
+                   List<Match> iterMatch = screen.findAllList(new Pattern(img));
+                   Match matchImage = iterMatch.get(1);
+                   matchImage.mouseMove();
+                   matchImage.click();
                 }else{
                     Match matchImage = screen.find(new Pattern(img));
                     matchImage.mouseMove();
@@ -46,9 +72,10 @@ public class umamusume {
                 System.out.println("Retrying to click the image..." + imgName);
             }
         }
+        stopWatch.stop();
     }
 
-    private static void doubleClickImage(Screen screen, String imgPath, String imgName) {
+    public static void doubleClickImage(Screen screen, String imgPath, String imgName) {
         String img = imgPath+imgName;
         boolean isClicked = false;
         while (!isClicked) {
@@ -61,11 +88,11 @@ public class umamusume {
         }
     }
 
-    private static void pasteRaidCode(Screen screen){
+    public static void pasteRaidCode(Screen screen){
         screen.type("v", Key.CTRL);
     }
 
-    private static boolean isExistScreen(Screen screen, String imgPath, String imgName){
+    public static boolean isExistScreen(Screen screen, String imgPath, String imgName){
         String img = imgPath + imgName;
         boolean isExist;
         try {
@@ -77,13 +104,17 @@ public class umamusume {
         return isExist;
     }
 
-    private static void clearPendingBattles(Screen screen,String strImagePath)throws Exception{
+    public static void clearPendingBattles(Screen screen,String strImagePath)throws Exception{
         clickImage(screen,strImagePath , "btnOK" + ".png");
         Thread.sleep(2000);
         while(isExistScreen(screen,strImagePath , "btnSelectSummon" + ".png")){
             clickImage(screen,strImagePath , "btnSelectSummon" + ".png");
-            clickImage(screen,strImagePath , "btnOK" + ".png");
+
+            if(isExistScreen(screen,strImagePath , "btnOK" + ".png")) {
+                clickImage(screen, strImagePath, "btnOK" + ".png");
+            }
             Thread.sleep(2000);
+
             if(isExistScreen(screen,strImagePath , "btnPendingBattle" + ".png")){
                 clickImage(screen,strImagePath , "btnPendingBattle" + ".png");
             }
@@ -98,5 +129,28 @@ public class umamusume {
         }
         return strTemp;
     }
+
+    public static void waitUntilImage(Screen screen, String imgPath, String imgName)throws Exception{
+        String img = imgPath+imgName;
+        boolean isExist = false;
+        while (!isExist) {
+            try {
+                Match matchImage = screen.find(new Pattern(img));
+                isExist = true;
+            }catch (FindFailed e) {
+                System.out.println("Retrying to click the image..." + imgName);
+            }
+        }
+    }
+
+    public static Properties loadConfig(Properties prop, String strConfigName){
+        try(FileInputStream fis = new FileInputStream(strConfigName)){
+            prop.load(fis);
+        }catch(Exception e){
+            System.out.println("FAILED READING CONFIG FILE...");
+        }
+        return prop;
+    }
+
     //end of line
 }
